60eaa1bfedb5fb063bd5c82e5a3f117ee5fdb31b
Andrew Diego Beers <andrew.d.beers@gmail.com>
2023-03-24 21:57:45 -0400

    Add project files.

---
+++ Benchmarks/ShufflingBenchmarks.cs
@@ -0,0 +1,76 @@
+﻿using BenchmarkDotNet.Attributes;
+using TheOmenDen.Shared.Extensions;
+namespace TheOmenDen.Shared.Benchmarks.Benchmarks;
+[MemoryDiagnoser]
+[ThreadingDiagnoser]
+public class ShufflingBenchmarks
+{
+    private readonly List<int> _smallestData = new (10);
+    private readonly List<int> _smallData = new (100);
+    private readonly List<int> _mediumData = new (1000);
+    private readonly List<int> _largeData = new (1000000);
+    
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            var words = new Bogus.Randomizer().Int();
+
+            _smallestData.Add(words);
+        }
+
+        for (var i = 0; i < 100; i++)
+        {
+            var words = new Bogus.Randomizer().Int();
+
+            _smallData.Add(words);
+        }
+
+        for (var i = 0; i < 1000; i++)
+        {
+            var words = new Bogus.Randomizer().Int();
+
+            _mediumData.Add(words);
+        }
+        for (var i = 0; i < 1000000; i++)
+        {
+            var words = new Bogus.Randomizer().Int();
+
+            _largeData.Add(words);
+        }
+    }
+
+    [Benchmark]
+    public Int32 GetRandomElementFromSmallestDataSet() => _smallestData.GetRandomElement();
+
+    [Benchmark]
+    public Int32 GetRandomElementFromSmallDataSet() => _smallData.GetRandomElement();
+
+    [Benchmark]
+    public Int32 GetRandomElementFromMediumDataSet() => _mediumData.GetRandomElement();
+
+    [Benchmark]
+    public Int32 GetRandomElementFromLargeDataSet() => _largeData.GetRandomElement();
+    
+    [Benchmark]
+    public List<Int32> GetRandomElementsFromSmallestDataSet() => _smallestData.GetRandomElements(5).ToList();
+
+    [Benchmark]
+    public List<Int32> GetRandomElementsFromSmallDataSet() => _smallData.GetRandomElements(5).ToList();
+
+    [Benchmark]
+    public List<Int32> GetRandomElementsFromMediumDataSet() => _mediumData.GetRandomElements(5).ToList();
+
+    [Benchmark]
+    public List<Int32> GetRandomElementsFromLargeDataSet() => _largeData.GetRandomElements(5).ToList();
+
+    [GlobalCleanup]
+    public void Cleanup()
+    {
+        _smallestData.Clear();
+        _smallData.Clear();
+        _mediumData.Clear();
+        _largeData.Clear();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Running;
+using Serilog;
+using Serilog.Events;
+using TheOmenDen.Shared.Benchmarks.Benchmarks;
+using TheOmenDen.Shared.Extensions;
+
+var logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+    .Enrich.FromLogContext()
+    .Enrich.WithThreadId()
+    .Enrich.WithThreadName()
+    .Enrich.WithProcessId()
+    .Enrich.WithProcessName()
+    .Enrich.WithMemoryUsage()
+    .WriteTo.Async(a =>
+    {
+        a.File("./logs/log-txt", rollingInterval: RollingInterval.Day);
+        a.Console();
+    })
+    .CreateLogger();
+
+try
+{
+    //var testEnumeration = Enumerable.Range(1, 10).ToArray();
+
+    //var randomElement = testEnumeration.GetRandomElementFromArray();
+    //var randomElements = testEnumeration.GetRandomElementsFromArray(5);
+
+    //Console.WriteLine($"randomElement {randomElement}");
+
+    //var elementPrint = string.Join(", ", randomElements);
+    //Console.WriteLine($"random elements: [{elementPrint}]");
+    _ = BenchmarkRunner.Run<ShufflingBenchmarks>();
+}
+catch (Exception ex)
+{
+    Log.Fatal("An error occurred before {AppName} could launch: {@Ex}", "Benchmarks", ex);
+}
+finally
+{
+    Log.CloseAndFlush();
+}
